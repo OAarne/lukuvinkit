7ffe859d1776732b65782478ffc5d147e856b9ac
Iikka Hauhio <iikka@kaivos.org>
2018-11-15 17:18:04 +0200

    sijoitettiin Command-enum omaan tiedostoonsa

---
+++ src/main/java/lukuvinkit/Command.java
@@ -0,0 +1,57 @@
+package lukuvinkit;
+
+import java.util.function.BiConsumer;
+
+public enum Command {
+    HELP("ohje", "tulostaa ohjeen", Command::printHelpImplementation),
+    CREATE("lisää", "lisää uuden lukuvinkin", Command::addReadingTipImplementation);
+    
+    private String commandString;
+    private String helpText;
+    private BiConsumer<CommandInterpreter, String[]> handler;
+    
+    private Command(String command, String helpText, BiConsumer<CommandInterpreter, String[]> handler) {
+        this.commandString = command;
+        this.helpText = helpText;
+        this.handler = handler;
+    }
+
+    public String getCommandString() {
+        return this.commandString;
+    }
+
+    public String getHelpText() {
+        return this.helpText;
+    }
+
+    public BiConsumer<CommandInterpreter, String[]> getHandler() {
+        return this.handler;
+    }
+
+    // Command methods
+
+    public static void printHelpImplementation(CommandInterpreter interpreter, String[] args) {
+        System.out.println("Tuetut komennot:");
+        for (Command cmd : Command.values()) {
+            System.out.println(cmd.commandString + " - " + cmd.helpText);
+        }
+    }
+
+    public static void addReadingTipImplementation(CommandInterpreter interpreter, String[] args) {
+        String title, description;
+        if (args.length == 3) {
+            title = args[1];
+            description = args[2];
+        }
+        else if (args.length == 1) {
+            title = interpreter.prompt("Otsikko> ", "Nimetön vinkki");
+            description = interpreter.prompt("Kuvaus> ", "");
+        }
+        else {
+            System.err.println("Lisää-komennolle annettiin väärä määrä argumentteja!");
+            return;
+        }
+        ReadingTip tip = new ReadingTip(title, description, null, null, null);
+        interpreter.getStorage().addEntry(tip);
+    }
+}
--- src/main/java/lukuvinkit/CommandInterpreter.java
+++ src/main/java/lukuvinkit/CommandInterpreter.java
@@ -5,54 +5,19 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiConsumer;
 
 public class CommandInterpreter implements AutoCloseable {
     private static Map<String, Command> COMMANDS = new HashMap<>();
+    
     static {
         for (Command command : Command.values()) {
-            COMMANDS.put(command.commandString, command);
-        }
-    }
-    
-    public static enum Command {
-        HELP("ohje", "tulostaa ohjeen", (interpreter, args) -> {
-            System.out.println("Tuetut komennot:");
-            for (Command cmd : Command.values()) {
-                System.out.println(cmd.commandString + " - " + cmd.helpText);
-            }
-        }),
-        CREATE("lisää", "lisää uuden lukuvinkin", (interpreter, args) -> {
-            String title, description;
-            if (args.length == 3) {
-                title = args[1];
-                description = args[2];
-            }
-            else if (args.length == 1) {
-                title = interpreter.prompt("Otsikko> ", "Nimetön vinkki");
-                description = interpreter.prompt("Kuvaus> ", "");
-            }
-            else {
-                System.err.println("Lisää-komennolle annettiin väärä määrä argumentteja!");
-                return;
-            }
-            ReadingTip tip = new ReadingTip(title, description, null, null, null);
-            interpreter.storage.addEntry(tip);
-        });
-        
-        private String commandString;
-        private String helpText;
-        private BiConsumer<CommandInterpreter, String[]> handler;
-        
-        private Command(String command, String helpText, BiConsumer<CommandInterpreter, String[]> handler) {
-            this.commandString = command;
-            this.helpText = helpText;
-            this.handler = handler;
+            COMMANDS.put(command.getCommandString(), command);
         }
     }
 
     private Storage storage;
     private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    
     public CommandInterpreter(Storage storage) {
         this.storage = storage;
     }
@@ -60,8 +25,19 @@ public class CommandInterpreter implements AutoCloseable {
     public void close() throws IOException {
         this.reader.close();
     }
+
+    public Storage getStorage() {
+        return this.storage;
+    }
     
-    private String prompt(String prompt, String defaultValue) {
+    /**
+     * Kysyy käyttäjältä syötteen annetulla kehotteella.
+     * 
+     * @param prompt kehote
+     * @param defaultValue Tämä arvo palautetaan, jos sisääntulovirta on suljettu.
+     * @return Käyttäjän syötteen.
+     */
+    public String prompt(String prompt, String defaultValue) {
         System.out.print(prompt);
         System.out.flush();
         try {
@@ -69,13 +45,13 @@ public class CommandInterpreter implements AutoCloseable {
             return input == null ? defaultValue : input;
         } catch (IOException e) {
             System.err.println("Virhe syötteen luvussa!");
-            return "";
+            return defaultValue;
         }
     }
     
     public void mainLoop() {
         System.out.println(COMMANDS.values().size());
-        loop: while (true) {
+        while (true) {
             String command = prompt("> ", "lopeta");
             if (command.equals("lopeta") || command.equals("poistu")) {
                 break;
@@ -83,7 +59,7 @@ public class CommandInterpreter implements AutoCloseable {
                 String[] args = command.split(" ");
                 Command cmdObj = COMMANDS.get(args[0]);
                 if (cmdObj != null) {
-                    cmdObj.handler.accept(this, args);
+                    cmdObj.getHandler().accept(this, args);
                 }
                 else {
                     System.err.println("Tuntematon komento! Syötä \"ohje\" saadaksesi lisätietoja.");
