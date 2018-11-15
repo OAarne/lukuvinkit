351f5b2e4073a853910e63a333bab3da61822195
Tuupertunut <superemppu@live.fi>
2018-11-15 16:57:55 +0200

    Korjattu lastReadingTipId:n kasvatus.

--- src/main/java/lukuvinkit/Storage.java
+++ src/main/java/lukuvinkit/Storage.java
@@ -1,29 +1,28 @@
 package lukuvinkit;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Storage {
-    
+
     private HashMap<Integer, ReadingTip> readingTips;
     int lastReadingTipId;
-    
+
     public Storage() {
         this.readingTips = new HashMap<>();
         this.lastReadingTipId = -1;
     }
-    
+
     public void addEntry(ReadingTip readingTip) {
         //The first reading tip added will have an id of 0. This ticks up by one
         //for each new tip added so that each id is unique.
         readingTips.put(lastReadingTipId + 1, readingTip);
+        lastReadingTipId++;
     }
-        
+
     public void removeReadingTipById(int id) {
         readingTips.remove(id);
     }
-    
+
     public ReadingTip getReadingTipById(int id) {
         return readingTips.get(id);
     }
