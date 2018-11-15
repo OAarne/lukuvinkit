43331b45767e271e9ce0509916f0c2c548c0584a
Iikka Hauhio <iikka@kaivos.org>
2018-11-15 16:02:23 +0200

    storage + entry

---
+++ src/main/java/lukuvinkit/Entry.java
@@ -0,0 +1,12 @@
+package lukuvinkit;
+
+public class Entry {
+    private int id;
+    private String title;
+    private String description;
+    
+    public Entry(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
---
+++ src/main/java/lukuvinkit/Storage.java
@@ -0,0 +1,7 @@
+package lukuvinkit;
+
+public class Storage {
+    public void addEntry(Entry entry) {}
+    public void removeEntryById(int id) {}
+    public Entry getEntryById(int id) {}
+}
