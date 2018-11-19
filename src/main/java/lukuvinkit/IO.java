5fc6d59edb7734fc4b7d2a3e86f0d833d4553b48
perander <silva.perander@helsinki.fi>
2018-11-19 18:03:18 +0200

    lisättiin rivit

--- src/main/java/lukuvinkit/IO.java
+++ src/main/java/lukuvinkit/IO.java
@@ -3,9 +3,14 @@ package lukuvinkit;
 import java.io.IOException;
 
 public interface IO extends AutoCloseable {
+
     void print(Object toPrint);
+
     void println(Object printLine);
+
     void println();
+
     String readLine() throws IOException;
+
     void flush();
 }
