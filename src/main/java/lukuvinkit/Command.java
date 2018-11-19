447c226b2a8f9bc6d516293136bae2564c658181
Iikka Hauhio <iikka@kaivos.org>
2018-11-19 17:07:07 +0200

    lisätty listaa + generalisoitu ReadingTip

--- src/main/java/lukuvinkit/Command.java
+++ src/main/java/lukuvinkit/Command.java
@@ -5,7 +5,8 @@ import java.util.function.BiConsumer;
 public enum Command {
 
     HELP("ohje", "tulostaa ohjeen", Command::printHelpImplementation),
-    CREATE("lisää", "lisää uuden lukuvinkin", Command::addReadingTipImplementation);
+    CREATE("lisää", "lisää uuden lukuvinkin", Command::addReadingTipImplementation),
+    LIST("listaa", "listaa olemassaolevat lukuvinkit", Command::listReadingTipsImplementation);
 
     private String commandString;
     private String helpText;
@@ -39,19 +40,33 @@ public enum Command {
     }
 
     public static void addReadingTipImplementation(CommandInterpreter interpreter, String[] args) {
-        String title, description;
+        ReadingTipField[] fields = ReadingTipField.values();
+        ReadingTip tip = new ReadingTip();
         if (args.length == 3) {
-            title = args[1];
-            description = args[2];
+            int i = 1;
+            for (ReadingTipField field : fields) {
+                tip.setFieldValue(field, args[i++]);
+            }
         } else if (args.length == 1) {
-            title = interpreter.prompt("Otsikko> ", "Nimetön vinkki");
-            description = interpreter.prompt("Kuvaus> ", "");
+            for (ReadingTipField field : fields) {
+                tip.setFieldValue(field, interpreter.prompt(field.getName() + "> ", ""));
+            }
         } else {
             System.err.println("Lisää-komennolle annettiin väärä määrä argumentteja!");
             return;
         }
-        ReadingTip tip = new ReadingTip(title, description, null, null, null);
         int id = interpreter.getStorage().addReadingTip(tip);
         System.out.println("Lisättiin vinkki tunnisteella " + id + ".");
+    }
+
+    public static void listReadingTipsImplementation(CommandInterpreter interpreter, String[] args) {
+        interpreter.getStorage().getReadingTips().forEach(entry -> {
+            System.out.print(entry.getKey());
+            ReadingTip tip = entry.getValue();
+            for (ReadingTipField field : ReadingTipField.values()) {
+                System.out.print(" | " + tip.getFieldValue(field));
+            }
+            System.out.println();
+        });
     }
 }
--- src/main/java/lukuvinkit/Storage.java
+++ src/main/java/lukuvinkit/Storage.java
@@ -1,21 +1,24 @@
 package lukuvinkit;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Storage {
 
-    private HashMap<Integer, ReadingTip> readingTips;
+    private LinkedHashMap<Integer, ReadingTip> readingTips;
     int lastReadingTipId;
 
     public Storage() {
-        this.readingTips = new HashMap<>();
-        this.lastReadingTipId = -1;
+        this.readingTips = new LinkedHashMap<>();
+        this.lastReadingTipId = 0;
     }
 
     public int addReadingTip(ReadingTip readingTip) {
         //The first reading tip added will have an id of 0. This ticks up by one
         //for each new tip added so that each id is unique.
-        readingTips.put(lastReadingTipId + 1, readingTip);
+        readingTips.put(lastReadingTipId, readingTip);
         return lastReadingTipId++;
     }
 
@@ -25,5 +28,9 @@ public class Storage {
 
     public ReadingTip getReadingTipById(int id) {
         return readingTips.get(id);
+    }
+
+    public List<Map.Entry<Integer, ReadingTip>> getReadingTips() {
+        return new ArrayList<>(readingTips.entrySet());
     }
 }
